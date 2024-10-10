2d811faf7e90e7f7b7afab836c33eaa311d31aa6
deca-francesco <francescodecarlo70@gmail.com>
2024-10-10 20:43:08 +0200

    UPDATE: array iteration and ADD results

--- main.js
+++ main.js
@@ -8,31 +8,59 @@ NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che 
 
 const randomNumbersEl = document.getElementById("random_numbers");
 const formEl = document.getElementById("numbers_form");
-// let inputsEl = document.querySelectorAll("input");
-// console.log(inputsEl);
-// let btn_submitEl = document.getElementById("btn_submit");
+const inputEl1 = document.getElementById("number1");
+const inputEl2 = document.getElementById("number2");
+const inputEl3 = document.getElementById("number3");
+const inputEl4 = document.getElementById("number4");
+const inputEl5 = document.getElementById("number5");
 const resultsEl = document.getElementById("results");
+let random_numbers_array = [];
+let your_numbers_array = [];
 
 function generate5RandomIntegers() {
-    let random_number_array = [];
     for (let i = 1; i <= 5; i++) {
         let random_number = Math.floor(Math.random() * 100) + 1;
-        random_number_array.push(random_number);
+        random_numbers_array.push(random_number);
     }
-    return random_number_array.join(" | ");
+    return random_numbers_array;
 }
 
-randomNumbersEl.innerHTML = generate5RandomIntegers();
-console.log(randomNumbersEl.innerHTML);
+random_numbers_array = generate5RandomIntegers();
+randomNumbersEl.innerHTML = random_numbers_array;
+console.log(random_numbers_array);
 
 const clock = setTimeout(function() {
     randomNumbersEl.classList.add("d-none");
     formEl.classList.remove("d-none");
-}, 3000);
+}, 5000);
 
 formEl.addEventListener("submit", function(e) {
     e.preventDefault();
     formEl.classList.add("d-none");
     resultsEl.classList.remove("d-none");
-    
+    your_numbers_array.push(Number(inputEl1.value), Number(inputEl2.value), Number(inputEl3.value), Number(inputEl4.value), Number(inputEl5.value));
+    let guessed_array = [];
+    let guessed_number = 0;
+    console.log(your_numbers_array);
+    for (let i = 0; i < your_numbers_array.length; i++) {
+        if (random_numbers_array.includes(your_numbers_array[i]) ) {
+            guessed_array.push(your_numbers_array[i]);
+            guessed_number++;
+        }
+    }
+
+    resultsEl.innerHTML = (`
+    <div id="your_numbers">
+        <h4>I tuoi numeri</h4>
+        ${your_numbers_array}
+    </div>
+    <div id="simon_numbers">
+        <h4>I numeri di Simon</h4>
+        ${random_numbers_array}
+    </div>
+    <div id="guessed" class="mt-5 flex-grow-1">
+        <h4>Numeri esatti: ${guessed_number}</h4>
+        ${guessed_array} 
+    </div>
+    `)
 })
